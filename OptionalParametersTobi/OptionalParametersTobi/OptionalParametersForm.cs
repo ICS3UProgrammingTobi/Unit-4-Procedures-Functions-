8ea227d0cd1a35c67dbd8ecf6df570bfb6f40bca
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-11-08 13:08:37 -0500

    p

--- OptionalParametersTobi/OptionalParametersTobi/OptionalParametersForm.cs
+++ OptionalParametersTobi/OptionalParametersTobi/OptionalParametersForm.cs
@@ -22,10 +22,27 @@ namespace OptionalParametersTobi
         //Description: This procedure displays a message box with the given five parameters: apptNumber,streetAdress,City,Province,postalCode
         public void PrintAdress(string apptNumber,string streetAdress,string City,string Province,string postalCode)
         {
-            Message box show 
+            MessageBox.Show("Your Appartement Number is:" +  apptNumber +  "Your Street Name is:" +  streetAdress  +  "The name of your City is:"  +  City  +  "Your province is:"  +  Province  +  "Your postal code is:"  +  postalCode);
+        }
+        //Procedure: PrintAdress
+        //Input: String apptNumber,String streetAdress,String City,String Province,String postalCode.
+        //Ouput; Void
+        //Description: This procedure displays a message box with the given four parameters: streetAdress,City,Province,postalCode
+        public void PrintAdress(string streetAdress, string City, string Province, string postalCode)
+        {
+            MessageBox.Show("Your Street Name is:" + streetAdress + "The name of your City is:" + City + "Your province is:" + Province + "Your postal code is:" + postalCode);
         }
         private void BtnEnter_Click(object sender, EventArgs e)
         {
+            //Declare local variables
+            string UserAppartementNumber, UserStreetAdress, Usercity, UserProvince,UserPostalCode;
+
+            //Get Inputs from message box
+            UserAppartementNumber = this.txtAptNumber.Text;
+            UserStreetAdress = this.txtStreetAdress.Text;
+            Usercity = this.txtCity.Text;
+
+
 
         }
 
