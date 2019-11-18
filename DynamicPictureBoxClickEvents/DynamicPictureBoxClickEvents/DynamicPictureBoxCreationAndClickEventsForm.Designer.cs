a5efd28a1bff960a89fbbe8b3f36727eef4aca84
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-11-18 13:09:19 -0500

    p

--- DynamicPictureBoxClickEvents/DynamicPictureBoxClickEvents/DynamicPictureBoxCreationAndClickEventsForm.Designer.cs
+++ DynamicPictureBoxClickEvents/DynamicPictureBoxClickEvents/DynamicPictureBoxCreationAndClickEventsForm.Designer.cs
@@ -28,20 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(164, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(447, 25);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Click on a picture to make the man move.";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(359, 209);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
             // frmDynamicPictureBoxCreationAndClickEvents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblInstructions);
             this.Name = "frmDynamicPictureBoxCreationAndClickEvents";
             this.Text = "Dynamic Picture Box Creation & Click Events";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- DynamicPictureBoxClickEvents/DynamicPictureBoxClickEvents/DynamicPictureBoxCreationAndClickEventsForm.cs
+++ DynamicPictureBoxClickEvents/DynamicPictureBoxClickEvents/DynamicPictureBoxCreationAndClickEventsForm.cs
@@ -15,6 +15,32 @@ namespace DynamicPictureBoxClickEvents
         public frmDynamicPictureBoxCreationAndClickEvents()
         {
             InitializeComponent();
+            //Hide the instructions label
+            this.lblInstructions.Hide();
+        }
+
+        private void GeneratePictureBoxes()
+        {
+            //Generate the picture boxes
+            GeneratePictureBoxes(61, 78);
+            GeneratePictureBoxes(464, 78);
+            GeneratePictureBoxes(61, 358);
+            GeneratePictureBoxes(464, 358);
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            //show the label
+            this.lblInstructions.Show();
+
+            //generate the picture boxes with their event listeners
+            GeneratePictureBoxes();
+        }
+
+        private void GeneratePictureBoxes(int x, int y)
+        {
+            // dynamically generate a new picture box and a new point at the given location (x,y)
+
         }
     }
 }
