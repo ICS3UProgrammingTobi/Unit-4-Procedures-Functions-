426df071add6435aceea28f09c64ded36d157e0b
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-11-18 12:44:08 -0500

    p

---
+++ DynamicPictureBoxClickEvents/DynamicPictureBoxClickEvents/DynamicPictureBoxCreationAndClickEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicPictureBoxClickEvents
+{
+    static class DynamicPictureBoxCreationAndClickEvents
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmDynamicPictureBoxCreationAndClickEvents());
+        }
+    }
+}
